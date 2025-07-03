f6b21c46e3145475780f1f2502991e370a839c49
lindsey5 <lindseysamson5@gmail.com>
2025-07-04 03:11:59 +0800

    initial commit

--- app.py
+++ app.py
@@ -7,7 +7,19 @@ app = Flask(__name__)
 # Register blueprint
 app.register_blueprint(predict_bp)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+# Allow multiple origins
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "http://localhost:5173", 
+            "https://kdmotoshop.onrender.com", 
+        ]
+    }
+})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    @app.route('/')
+    def home():
+        return 'Hello from KDMotoshop on Render!'
