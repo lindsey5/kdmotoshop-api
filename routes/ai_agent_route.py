9a97ec189f315b0eefb3645811f1353934dcdab4
lindsey5 <lindseysamson5@gmail.com>
2025-07-29 13:05:33 +0800

    commit

--- routes/ai_agent_route.py
+++ routes/ai_agent_route.py
@@ -6,7 +6,7 @@ import uuid
 agent_bp = Blueprint("agent", __name__)
 
 @agent_bp.route("/api/chat", methods=['POST', 'OPTIONS'])
-@cross_origin(origins=["https://kdmotoshop.onrender.com", "http://localhost:5173"]) 
+@cross_origin(origins=["https://kdmotoshop.onrender.com", "http://localhost:5173"], allow_headers=['Content-Type', 'Authorization']) 
 def chat():
     if request.method == 'OPTIONS':
         # Preflight request
