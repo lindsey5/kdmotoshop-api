dd19ec7aa23e2794896a7fdfbe872c12683d8c5f
lindsey5 <lindseysamson5@gmail.com>
2025-07-30 19:21:09 +0800

    initial commit

--- agent/config.py
+++ agent/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 
 load_dotenv()
-print(os.environ.get("GEMINI_API_KEY"))
+
 os.environ["GOOGLE_API_KEY"] = os.environ.get("GEMINI_API_KEY")
 
 model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
