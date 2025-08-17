2ad019c9119364c24f532e25bbab5d7eda364b02
lindsey5 <lindseysamson5@gmail.com>
2025-08-17 17:46:13 +0800

    initial commit

--- agent/tools.py
+++ agent/tools.py
@@ -1,8 +1,8 @@
 from langchain.tools import tool
 import os
-
 from agent.db import get_products_collection
-from .utils import _format_product, create_rag_chain
+from .utils import _format_product
+from .chain import create_rag_chain
 from agent.vector import create_pdf_vectorstore
 
 url = os.environ.get("URL")
