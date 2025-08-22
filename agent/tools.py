44d810d8e74ab6857ad393a96fcdb81c089e7d83
lindsey5 <lindseysamson5@gmail.com>
2025-08-23 04:22:09 +0800

    initial commit

--- agent/tools.py
+++ agent/tools.py
@@ -1,5 +1,6 @@
-from langchain.tools import tool
 import os
+import requests 
+from langchain.tools import tool
 from agent.db import get_products_collection
 from .utils import _format_product
 from .chain import create_rag_chain
@@ -9,13 +10,15 @@ url = os.environ.get("URL")
 
 # vectorstore = create_pdf_vectorstore("data/qa.pdf")
 vectorstore = load_vectorstore()
-qa_chain =  create_rag_chain(vectorstore)
+qa_chain = create_rag_chain(vectorstore)
+
 
 @tool
 def ask_question(question: str) -> str:
-    """Search for a knowledge based answers related to customer questions"""
+    """Search for knowledge-based answers related to customer questions"""
     result = qa_chain({"query": question})
     return result["result"]
+
 
 @tool
 def products_tool() -> str:
@@ -23,9 +26,36 @@ def products_tool() -> str:
     products = list(get_products_collection().find({}))
     if not products:
         return "No products found."
-    
+
     return [_format_product(product) for _, product in enumerate(products)]
 
+
+@tool
+def get_top_products() -> str:
+    """Fetch the most selling products from the API."""
+    try:
+        response = requests.get(f"{url}/api/products/top")
+        response.raise_for_status()  # raise error if status != 200
+
+        data = response.json()
+        products = data.get("topProducts", [])
+
+        if not products:
+            return "No top products found."
+
+        result = ""
+        for p in products:
+            result += (
+                f'Product: {p["product_name"]}\n'
+                f'Price: {p["price"]}\n'
+                f'Quantity Sold: {p["totalQuantity"]}\n'
+                f'Rating: {p["rating"]}\n\n'
+            )
+        return result.strip()
+
+    except Exception as e:
+        return f"Error fetching top products: {str(e)}"
+
+
 def getTools():
-    tools = [products_tool, ask_question]
-    return tools
+    return [products_tool, ask_question, get_top_products]
