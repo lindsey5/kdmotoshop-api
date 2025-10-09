f894d84de6e72c097d8039fc17458407043f7713
lindsey5 <lindseysamson5@gmail.com>
2025-10-09 16:51:29 +0800

    Remove main entry point for uvicorn server

--- main.py
+++ main.py
@@ -26,8 +26,4 @@ app.include_router(agent_router)
 async def run():
     return JSONResponse(content={"response": "Hi."})
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
 handler = Mangum(app)
